effa17d0040096fca1085a993419d5bfcb5adf9b
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2025-04-27 08:47:52 +0100

    refactor: config tsup and some cleaning

---
+++ tsup.config.ts
@@ -0,0 +1,32 @@
+import {defineConfig, Options} from "tsup";
+
+export const defaultOptions = {
+    format: ['cjs', 'esm'],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    shims: true,
+    outDir: 'lib',
+    entry: ['src/**/*.ts', '!src/**/*.test.ts'],
+    bundle: false,
+    treeshake: false,
+};
+
+/**
+ *
+ */
+export function fromPackageJson(packageJson: {
+    exports?: { [key: string]: { import: string } };
+}): string[] {
+    if (!packageJson.exports) return ['./src/index.ts'];
+    const out: string[] = [];
+    for (const key in packageJson.exports) {
+        if (Object.prototype.hasOwnProperty.call(packageJson.exports, key)) {
+            const importFile = packageJson.exports[key].import;
+            out.push(importFile.replace('./lib', './src').replace('.mjs', '.ts'));
+        }
+    }
+    return out;
+}
+
+export default defineConfig({...(defaultOptions as Options),});
